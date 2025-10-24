900fcb48c2d22230b61b247641d201e276143aef
leop4-cod <leo0411201@gmail.com>
2025-10-24 16:35:17 -0500

    CLASE 24-10-2025

---
+++ calidad_software_manana-jest/modulo_1/git/andres.py
@@ -0,0 +1,11 @@
+print("Hola mundo desde andres.py")
+print("Estoy aprendiendo git")
+print("Estoy aprendiendo a usar ramas en git")
+print("Estoy aprendiendo a usar git desde vs code")
+print("Estoy aprendiendo a usar git desde andres.py")
+
+suma = 0
+a= 9
+b= 10
+suma = a + b
+print("El resultado de la suma es: ", suma)
---
+++ calidad_software_manana-jest/modulo_1/git/myapp.py
@@ -0,0 +1,4 @@
+print("PRUEBA DESDE CALIDAD DE SOFWARE")
+materia= "Calidad de Software"
+if(materia):
+    print("Estoy practicando git en: ", materia)
